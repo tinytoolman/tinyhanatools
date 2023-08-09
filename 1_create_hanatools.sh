78a24d46913eade1351463f4a544179953d8c9f0
Tiny Toolman <141386392+tinytoolman@users.noreply.github.com>
2023-08-08 23:09:54 -0500

    Update 1_create_hanatools.sh

--- 1_create_hanatools.sh
+++ 1_create_hanatools.sh
@@ -1,4 +1,4 @@
-'''sh
+```sh
 rm -r __pycache__/ build/ dist A0_tinyhanatools.spec
 sleep 1
 pyinstaller --onefile --hidden-import=hdbcli A0_tinyhanatools.py
@@ -12,3 +12,4 @@ chown sidadm:sapsys /tinyhanatools/dist/A0_tinyhanatools
 sleep 1
 #PLEASE CHANGE SID AND INSTANCE NUMBER BELOW!!!
 cp -p /tinyhanatools/dist/A0_tinyhanatools /usr/sap/SID/HDB??/exe/tinyhanatools
+```

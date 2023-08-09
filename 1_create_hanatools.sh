7cc3d7826da2308d6ecde5f5a468ac3aa9cd93d6
Tiny Toolman <141386392+tinytoolman@users.noreply.github.com>
2023-08-08 23:06:30 -0500

    Create 1_create_hanatools.sh

---
+++ 1_create_hanatools.sh
@@ -0,0 +1,13 @@
+rm -r __pycache__/ build/ dist A0_tinyhanatools.spec
+sleep 1
+pyinstaller --onefile --hidden-import=hdbcli A0_tinyhanatools.py
+sleep 1
+#PLEASE CHANGE SID BELOW!!!
+chown sidadm:sapsys /tinyhanatools/dist/A0_tinyhanatools
+sleep 1
+chmod +x /tinyhanatools/dist/A0_tinyhanatools
+sleep 1
+chown sidadm:sapsys /tinyhanatools/dist/A0_tinyhanatools
+sleep 1
+#PLEASE CHANGE SID AND INSTANCE NUMBER BELOW!!!
+cp -p /tinyhanatools/dist/A0_tinyhanatools /usr/sap/SID/HDB??/exe/tinyhanatools
